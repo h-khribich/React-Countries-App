a466ce7ed600f96f2ec09dd1e0f88a1dd390d64f
h-khribich <hamza.khribich@gmail.com>
2021-12-09 23:42:41 +0100

    Images implemented

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="../assets/logo512.png" alt="Logo de React" />
+      <h3>React World</h3>
+    </div>
+  );
+};
+
+export default Logo;
