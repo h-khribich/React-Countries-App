4985345dbcc9e52dd27e3999de7a4f29442e2a47
h-khribich <hamza.khribich@gmail.com>
2021-12-09 01:56:03 +0100

    Active link nav added & router downgrade

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -7,11 +7,11 @@ import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="a-propos" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/a-propos" exact component={About} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 };
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <NavLink to="/" exact activeClassName="nav-active">
+        Accueil
+      </NavLink>
+      <NavLink to="a-propos" exact activeClassName="nav-active">
+        À propos
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navigation;
